054edf13c289dbd09d8a4d4b0596964ea067835d
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-05-08 23:28:30 +0800

    聚合程序

---
+++ spring-boot-war-demo/src/main/java/com/zhou/springbootwardemo/SpringBootWarDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zhou.springbootwardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWarDemoApplication.class, args);
+	}
+}
---
+++ spring-boot-war-demo/src/test/java/com/zhou/springbootwardemo/SpringBootWarDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhou.springbootwardemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWarDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

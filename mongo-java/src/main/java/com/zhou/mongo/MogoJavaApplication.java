c213377f1f6d4a765896126f4e6a0e67bde6dcdd
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-05-16 23:25:05 +0800

    搭建spring boot的mongodb环境

---
+++ mongo-java/src/main/java/com/zhou/mongo/MogoJavaApplication.java
@@ -0,0 +1,20 @@
+/**
+ * Date:     2018/5/1623:18
+ * AUTHOR:   Administrator
+ */
+package com.zhou.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot mongo 启动类
+ * 2018/5/16  23:18
+ * created by zhoumb
+ */
+@SpringBootApplication
+public class MogoJavaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MogoJavaApplication.class, args);
+    }
+}
